a4d67a533ecb214542aba6421ec2a650ce341fe4
Bradley Sweet <sweet.brad@gmail.com>
2026-04-15 21:38:20 -0400

    Removal of OCR content

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+/*
+ * The following code is derived from work by mort13.
+ * Copyright (c) 2026 mort13
+ * Licensed under the MIT License. See the LICENSE-THIRD-PARTY.txt file in the project root for full license information.
+ */
+
 import { loadMiningData } from './data-manager.js';
 import { setupChart } from './chart-manager.js';
 import { setupLaserheadUI } from './laserhead-manager.js';
 import { setupModuleUI } from './module-manager.js';
 import { setupTabs, setupLaserheadButtons, setupDarkModeToggle, injectFooterToTabs } from './ui-manager.js';
 import { setupGadgetUI } from './gadget-manager.js';
-import { setupOCR } from './ocr-ui.js';
 import { setupSettingsUI } from './settings-manager.js';
 import { setupScanUI } from './scan-manager.js';
 
@@ -19,7 +24,6 @@ window.addEventListener("DOMContentLoaded", async () => {
     setupModuleUI();
     setupLaserheadButtons();
     setupGadgetUI();
-    setupOCR();
     setupSettingsUI();
     setupScanUI();
 });
